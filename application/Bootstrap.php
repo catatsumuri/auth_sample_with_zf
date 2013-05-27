b0e11471344d7e2a451058181a1f14a411000b63
catatsumuri <catatsumuri@gmail.com>
2013-05-27 17:41:06 +0900

    update view and initDoctype

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,19 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+    protected function _initDoctype()
+    {
+        $this->bootstrap('view');
+        $view = $this->getResource('view');
+        $config = new Zend_Config_Ini(APPLICATION_PATH .
+                '/configs/application.ini', APPLICATION_ENV);
+        Zend_Registry::set("config", $config);
+        $view->doctype('HTML5');
+        $view->headMeta()->setCharset('UTF-8');
 
-
+        ZendX_JQuery::enableView($view);
+        $view->jQuery()->setVersion('1.7.1')->enable();
+    }
 }
 
+// vim:set ts=4 sts=4 sw=4 expandtab:
