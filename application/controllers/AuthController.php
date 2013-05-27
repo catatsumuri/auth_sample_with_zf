e00ab28e9349eabde25027f6bb3cfa854dab47b3
catatsumuri <catatsumuri@gmail.com>
2013-05-27 19:24:19 +0900

    add authentication method

--- application/controllers/AuthController.php
+++ application/controllers/AuthController.php
@@ -2,31 +2,75 @@
 
 class AuthController extends Zend_Controller_Action
 {
+    private $_userModel;
 
     public function init()
     {
-        /* Initialize action controller here */
+        $this->_userModel = new Application_Model_DbTable_User;
     }
 
-    public function indexAction()
+    private function _login()
     {
-        // action body
-    }
+        $username = $this->getRequest()->getPost('username');
+        $password = $this->getRequest()->getPost('password');
 
-    public function loginAction()
-    {
-        // action body
+        $notEmpty = new Zend_Validate_NotEmpty();
+        if ($notEmpty->isValid($username) === false) {
+            return "Login ID is empty";
+        }
+        if ($notEmpty->isValid($password) === false) {
+            return "Password is empty";
+        }
+
+        $adapter = new Zend_Auth_Adapter_DbTable();
+        $adapter->setTableName('user');
+        $adapter->setIdentityColumn('account');
+        $adapter->setCredentialColumn('password');
+        $adapter->setIdentity($username);
+        $adapter->setCredential(md5($password)); // or Using credentialTreatment
+
+        $auth    = Zend_Auth::getInstance();
+        $result  = $auth->authenticate($adapter);
+        if ($result->isValid()) {
+            $storage = $auth->getStorage();
+            $row = $adapter->getResultRowObject(array('id', 'account'));
+            $storage->write($row);
+            $this->_helper->redirector("index", "dashboard", "default");
+            // }}}
+        }
+        return "認証に失敗しました";
     }
 
     public function logoutAction()
     {
-        // action body
+        $auth = Zend_Auth::getInstance();
+        Zend_Session::destroy();
+        $this->_helper->redirector('login', 'auth', 'default');
     }
 
+    public function loginAction()
+    {
+        $error = null;
+        if ($this->getRequest()->isPost()) {
+            $error = $this->_login();
+        }
+        // Set path query into the view
+        if (!$path = $this->getRequest()->getQuery('path')) {
+            $path = $this->getRequest()->getPost('path');
+        }
+
+        $this->view->path  = $path;
+        $this->view->error = $error;
+    }
+
+    public function indexAction()
+    {
+        // Simply redirect to login form
+        $this->_helper->redirector("login", "auth", "default");
+    }
 
 }
-
-
-
-
-
+// {{{ Modeline
+// vim:set ts=4 sts=4 sw=4 expandtab:
+// vim600: fdm=marker
+// }}}
--- docs/schema.sql
+++ docs/schema.sql
@@ -4,5 +4,5 @@ CREATE TABLE user (
   , password VARCHAR(255)
   , UNIQUE(account)
 );
-INSET INTO user(null, "test1", "5a105e8b9d40e1329780d62ea2265d8a");
-INSET INTO user(null, "test2", "ad0234829205b9033196ba818f7a872b");
+INSERT INTO user VALUES(null, "test1", "5a105e8b9d40e1329780d62ea2265d8a");
+INSERT INTO user VALUES(null, "test2", "ad0234829205b9033196ba818f7a872b");
